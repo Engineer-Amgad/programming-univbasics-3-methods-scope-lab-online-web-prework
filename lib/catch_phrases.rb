7beeac0fdc7686b99d6d04c4df5cc1250551489d
fearless-jest-9632 <amgadmalik@yahoo.com>
2019-09-02 02:03:01 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,6 +9,12 @@ def toadstool
     status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
 def any_phrase (phrase)
+  puts phrase
+end
+
+def link
+  phrase = "It's Dangerous To Go Alone! Take This."
   puts phrase
 end
